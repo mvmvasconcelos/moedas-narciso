49b538ea8236b4b4223d99e790c9fb99aa492b5f
Vinicius Vasconcelos <www.vasconcelos@gmail.com>
2025-09-05 12:21:42 -0300

    Ajuste de metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,34 @@ import { AuthProvider } from '@/contexts/AuthContext';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Moedas Narciso',
-  description: 'Plataforma de gerenciamento de trocas de materiais recicláveis e Moedas Narciso.',
+  title: 'Moedas Narciso - Cooperativa do Bem',
+  description:
+    'Conheça o Moedas Narciso, um projeto da EMEF Narciso Mariante de Campos que une reciclagem, educação financeira e solidariedade com alunos do pré ao 5º ano.',
+  metadataBase: new URL('https://moedasnarciso.com.br'),
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    title: 'Moedas Narciso - Cooperativa do Bem',
+    description:
+      'Conheça o Moedas Narciso, um projeto da EMEF Narciso Mariante de Campos que une reciclagem, educação financeira e solidariedade com alunos do pré ao 5º ano.',
+    url: 'https://moedasnarciso.com.br',
+    siteName: 'Moedas Narciso',
+    type: 'website',
+    images: [
+      {
+        url: '/images/retangulo.png',
+        alt: 'Imagem retangular Moedas Narciso',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Moedas Narciso - Cooperativa do Bem',
+    description:
+      'Conheça o Moedas Narciso, um projeto da EMEF Narciso Mariante de Campos que une reciclagem, educação financeira e solidariedade com alunos do pré ao 5º ano.',
+  images: ['/images/retangulo.png'],
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +43,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
+      <head>
+        {/* JSON-LD básico para organização/site */}
+        <script
+          type="application/ld+json"
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'WebSite',
+              name: 'Moedas Narciso',
+              url: 'https://moedasnarciso.com.br',
+              description:
+                'Conheça o Moedas Narciso, um projeto da EMEF Narciso Mariante de Campos que une reciclagem, educação financeira e solidariedade com alunos do pré ao 5º ano.',
+              potentialAction: {
+                '@type': 'SearchAction',
+                target: 'https://moedasnarciso.com.br/?s={search_term_string}',
+                'query-input': 'required name=search_term_string',
+              },
+            }),
+          }}
+        />
+      </head>
       <body className="font-sans antialiased" suppressHydrationWarning>
         <AuthProvider>
           {children}
